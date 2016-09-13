12d1fcb598ee12b04c607381f24fb2bb7ba68116
reversearrow <reversearrow@gmail.com>
2016-09-13 14:15:05 -0400

    Added User Signup Form

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
-import cgi
+import cgi,re
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = re.compile("^.{3,20}$")
+EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+
+
 def rot13(text):
         alphabets = {"a":"n","b":"o","c":"p","d":"q","e":"r","f":"s","g":"t","h":"u","i":"v","j":"w","k":"x","l":"y","m":"z","n":"a","o":"b","p":"c","q":"d","r":"e","s":"f","t":"g","u":"h","v":"i","w":"j","x":"k","y":"l","z":"m"}
         alphabets_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m"]
@@ -20,3 +25,13 @@ def rot13(text):
                         rot13 += i
 
         return rot13
+
+
+def valid_username(username):
+    return USER_RE.match(username)
+
+def valid_password(password):
+    return PASSWORD_RE.match(username)
+
+def valid_email(email):
+    return EMAIL_RE.match(username)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import rot13
+from functions import rot13, valid_username, valid_password, valid_email
 import webapp2
 
 rot13_form = """
@@ -7,6 +7,20 @@ rot13_form = """
 <input type="text" name="text" value="%(text)s" style="height: 100px; width: 400px"></input><br>
 <input type="submit">
 </form>
+"""
+
+user_signup_form = """
+<h2>User Signup</h2>
+<form method="POST">
+<label> Username <input type = "text" name = "username" value = "%(username)s"> </input></label> <br>
+<label> Password <input type = "password" name = "password" value = "%(password)s"> </input></label> <br>
+<label> Verify <input type = "password" name = "verify"> </input></label> <br>
+<label> Email <input type = "text" name = "email" value = "%(email)s"> </input></label> <br>
+<input type="submit">
+"""
+
+welcome_message = """
+<h1> Welcome %(username)s. Thank you for signing up. </h1>
 """
 
 
@@ -25,4 +39,20 @@ class Rot13(webapp2.RequestHandler):
 		self.response.out.write(rot13_form % ({"text":rot13_text}))
 
 
-app = webapp2.WSGIApplication([('/', MainPage),('/unit2/rot13',Rot13)], debug=True)
+class Signup(webapp2.RequestHandler):
+	def write_form(self,username="",password="",email=""):
+		self.response.write(user_signup_form % ({"username":username,"password":password,"email":email}))
+	def get(self):
+		self.write_form()
+	def post(self):
+		username = self.request.get("username")
+		password = self.request.get("password")
+		verify = self.request.get("verify")
+		email = self.request.get("email")
+
+
+	def welcome_message(self,username):
+		self.response.out.write(welcome_message % ({"username": username}))
+
+
+app = webapp2.WSGIApplication([('/', MainPage),('/unit2/rot13',Rot13),('/unit2/signup',Signup)], debug=True)
