57ae3f937e00fc3688a07be348ba1008affe77d7
Jay Shah <reversearrow@gmail.com>
2016-09-13 09:07:07 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,19 +1,22 @@
 import cgi
 def rot13(text):
         alphabets = {"a":"n","b":"o","c":"p","d":"q","e":"r","f":"s","g":"t","h":"u","i":"v","j":"w","k":"x","l":"y","m":"z","n":"a","o":"b","p":"c","q":"d","r":"e","s":"f","t":"g","u":"h","v":"i","w":"j","x":"k","y":"l","z":"m"}
-	alphabets_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m"]
-	rot13 = ""
+        alphabets_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m"]
+        rot13 = ""
         test_text = cgi.escape(text,quote=True)
         print test_text
         for i in text:
-                if i != " ":
+                if i != " " and ord(i) > 63 and ord(i) < 123:
                         if i.islower():
                                 get_letter = alphabets[i]
                                 rot13 += get_letter
                         else :
                                 get_letter = alphabets[i.lower()]
                                 rot13 += get_letter.upper()
-                else:
+                elif i == " ":
                         rot13 += (" ")
-        return rot13
+                else:
+                        i = cgi.escape(i, quote=True)
+                        rot13 += i
 
+        return rot13
